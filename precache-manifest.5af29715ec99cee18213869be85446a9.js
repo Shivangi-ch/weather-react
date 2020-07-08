058589e4abf2c3aad98009ce21dfe19eee5ca2f1
Shivangi-ch <shivangi.ch.936@gmail.com>
2020-07-08 13:21:18 +0530

    Updates

---
+++ precache-manifest.5af29715ec99cee18213869be85446a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f3529433fd602f311ddb296df3fe1ca",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "3dce7585ca924549b00f",
+    "url": "/weather-react/static/css/main.4ea09036.chunk.css"
+  },
+  {
+    "revision": "4a79708fe2bb8d98f2f2",
+    "url": "/weather-react/static/js/2.07144284.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-react/static/js/2.07144284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dce7585ca924549b00f",
+    "url": "/weather-react/static/js/main.e71aed15.chunk.js"
+  },
+  {
+    "revision": "978dc9f2a9f9512ae584",
+    "url": "/weather-react/static/js/runtime-main.17cf43c5.js"
+  }
+]);
